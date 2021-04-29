28b2f475b40c86d9e3988e6eab12d42b692e1577
Selim Imoberdorf <selimob@gmail.com>
2021-04-29 16:56:38 +0200

    mobile nav

---
+++ components/NavLink.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+
+interface Props {
+  url: string;
+  title?: string;
+  children?: string;
+}
+
+const NavLink: React.FC<Props> = (props) => {
+  const { url, title, children } = props;
+  return (
+    <Link href={url} passHref>
+      {/* Todo if active, then text-indigo-700*/}
+      <a
+        className="flex flex-col flex-nowrap justify-center items-center h-10 px-5 font-sans font-medium text-sm md:text-sm-desktop text-gray-900 group hover:text-indigo-500 active:text-indigo-900 transition transition-colors"
+        title={title}
+      >
+        {children}
+        <span className="w-3 h-2px mt-0.5 bg-current opacity-0 group-hover:opacity-100 transition transition-opacity" />
+      </a>
+    </Link>
+  );
+};
+
+export default NavLink;
---
+++ components/NavMobile.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import LogoInverted from "@components/LogoInverted";
+import NavLink from "@components/NavLink";
+// @ts-ignore
+import IconMenu from "./icons/menu.svg";
+
+interface Props {}
+
+const NavMobile: React.FC<Props> = (props) => {
+  const {} = props;
+  return (
+    <nav className="fixed w-full h-screen z-50 overflow-hidden flex flex-col bg-white bg-opacity-40 backdrop-filter backdrop-blur-2xl">
+      <div className="flex flex-nowrap justify-between">
+        <LogoInverted />
+        <button className="flex-initial inline-flex items-center px-3 font-medium">
+          Menu <IconMenu className="flex-initial w-3 h-3 ml-1.5" />
+        </button>
+      </div>
+      <div className="flex-auto flex flex-col flex-nowrap justify-around items-center px-5 py-10">
+        <NavLink url={"/"}>Übersicht</NavLink>
+        <NavLink url={"/"}>Projekte</NavLink>
+        <NavLink url={"/"}>Team</NavLink>
+        <NavLink url={"/"}>Kontakt</NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default NavMobile;
