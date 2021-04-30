10cfe2893fbb9b5265c41d194651d8943ea1a06a
Selim Imoberdorf <selimob@gmail.com>
2021-04-30 10:00:08 +0200

    updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import NavMain from "@components/NavMain";
+import NavMobile from "@components/NavMobile";
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Konoma</title>
       </Head>
-      <NavMain />
+      <NavMobile />
     </>
   );
 }
