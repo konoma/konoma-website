f650f669513967314df92b2f7b9d56bc0b82727d
Selim Imoberdorf <selimob@gmail.com>
2021-05-06 15:20:55 +0200

    comp footer

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BaseComponentProps } from "helper/classNames";
 import Logo from "@components/Logo";
+import LinkIcon from "@components/LinkIcon";
+import { IconMail, IconArrow } from "@components/icons";
 
 interface SiteFooterProps extends BaseComponentProps {}
 
@@ -8,12 +10,31 @@ const SiteFooter: React.FC<SiteFooterProps> = (props) => {
   const { className } = props;
 
   return (
-    <footer className="flex flex-col mt-8 p-5 md:p-8 bg-white border-t border-indigo-200">
+    <footer
+      className="flex flex-col mt-8 p-5 md:p-8 space-y-4 \
+                bg-white border-t border-indigo-200"
+    >
       <Logo />
-      <div className="font-sans text-sm text-center">
+      <div className="font-sans font-medium text-sm text-center">
         Konoma GmbH, <br className="md:hidden" />
         Rütistrasse 6, <br className="md:hidden" />
         5400 Baden, Schweiz
+      </div>
+      <div className="flex flex-nowrap flex-col md:flex-row justify-center items-center">
+        <LinkIcon
+          text={"info@konoma.ch"}
+          to={"mailto:info@konoma.ch"}
+          type={"internal"}
+          className={`flex-initial p-0.5 md:p-2`}
+          iconLeft={<IconMail className={`flex-initial w-3 h-3 mr-1`} />}
+        />
+        <LinkIcon
+          text={"Kontakt"}
+          to={"/kontakt"}
+          type={"internal"}
+          className={`flex-initial p-0.5 md:p-2`}
+          iconLeft={<IconArrow className={`flex-initial w-3 h-3 mr-1`} />}
+        />
       </div>
     </footer>
   );
