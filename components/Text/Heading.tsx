5b80db940ee5c959b261648efc68aee44b05ad94
Selim Imoberdorf <selimob@gmail.com>
2021-05-10 16:19:09 +0200

    added modifier for indigo headings

--- components/Text/Heading.tsx
+++ components/Text/Heading.tsx
@@ -4,38 +4,40 @@ import { BaseComponentProps } from "../../helper/classNames";
 interface HeadingProps extends BaseComponentProps {
   type?: string;
   level: "hero" | "1" | "2" | "3" | "4";
+  colorBlue?: boolean;
 }
 const Heading: React.FC<HeadingProps> = (props) => {
-  const { className, level, type, children } = props;
+  const { className, level, type, children, colorBlue } = props;
   const Element = type ?? ("span" as React.ElementType);
 
   return (
     <Element
       className={`${
         level === "hero"
-          ? "font-sans font-semibold text-5xl lg:text-5xl-desktop text-gray-900"
+          ? "font-sans font-semibold text-5xl lg:text-5xl-desktop"
           : ""
       } \
        ${
          level === "1"
-           ? "font-sans font-semibold text-4xl lg:text-4xl-desktop text-gray-900"
+           ? "font-sans font-semibold text-4xl lg:text-4xl-desktop"
            : ""
        } \
        ${
          level === "2"
-           ? "font-serif font-bold text-3xl lg:text-3xl-desktop text-gray-900"
+           ? "font-serif font-bold text-3xl lg:text-3xl-desktop"
            : ""
        } \
        ${
          level === "3"
-           ? "font-sans font-semibold text-2xl lg:text-2xl-desktop text-gray-900"
+           ? "font-sans font-semibold text-2xl lg:text-2xl-desktop"
            : ""
        } \
        ${
          level === "4"
-           ? "font-sans font-semibold text-xl lg:text-xl-desktop text-gray-900"
+           ? "font-sans font-semibold text-xl lg:text-xl-desktop"
            : ""
        } \
+       ${colorBlue === true ? "text-indigo-700" : "text-gray-900"}
        ${className ?? ""}`}
     >
       {children}
