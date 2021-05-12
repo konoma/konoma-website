1e23c87630a4878a1349ff5a00ecd5e27d6d1b12
Selim Imoberdorf <selimob@gmail.com>
2021-05-12 16:57:22 +0200

    added sample images

--- components/ContentImg.tsx
+++ components/ContentImg.tsx
@@ -1,33 +1,34 @@
 import React from "react";
 import Image from "next/image";
-import {BaseComponentProps} from "../helper/classNames";
+import { BaseComponentProps } from "../helper/classNames";
 
 interface ContentImgProps extends BaseComponentProps {
-    imgUrl: string;
-    imgAlt?: string;
-    ratio: "4/3" | "2/1" | "1/1";
-    fill: "cover" | "contain";
+  imgUrl?: string;
+  imgAlt?: string;
+  ratio: "4/3" | "2/1" | "1/1";
+  fill: "cover" | "contain";
 }
 
 const ContentImg: React.FC<ContentImgProps> = (props) => {
-    const { className, imgUrl, imgAlt, ratio, fill } = props;
+  const { className, imgUrl, imgAlt, ratio, fill } = props;
 
-    return (
-        <div className={`${ratio === "4/3" ? "aspect-w-4 aspect-h-3" : ""}
+  return (
+    <div
+      className={`${ratio === "4/3" ? "aspect-w-4 aspect-h-3" : ""}
                         ${ratio === "2/1" ? "aspect-w-2 aspect-h-1" : ""}
                         ${ratio === "1/1" ? "aspect-w-1 aspect-h-1" : ""}
                         ${className ?? ""}        
-        `}>
-            <Image
-                src={imgUrl}
-                alt={imgAlt}
-                layout="fill"
-                className={`${fill === "cover" ? "object-cover" : ""}
+        `}
+    >
+      <Image
+        src={imgUrl}
+        alt={imgAlt}
+        layout="fill"
+        className={`${fill === "cover" ? "object-cover" : ""}
                             ${fill === "contain" ? "object-contain" : ""}`}
-            />
-        </div>
-    )
-
-}
+      />
+    </div>
+  );
+};
 
 export default ContentImg;
