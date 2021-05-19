0eaf21a960d50d5d2765f7e4d5ecdbe0b1d06ac6
Selim Imoberdorf <selimob@gmail.com>
2021-05-19 10:13:42 +0200

    added LinkCard props

--- components/Navigation/LinkCard.tsx
+++ components/Navigation/LinkCard.tsx
@@ -4,33 +4,36 @@ import { BaseComponentProps } from "../../helper/classNames";
 import Heading from "@components/Text/Heading";
 import { IconChevron } from "@components/icons";
 
-interface LinkCardProps extends BaseComponentProps {}
+interface LinkCardProps extends BaseComponentProps {
+  label: string;
+  title: string;
+  to: string;
+}
 
 const LinkCard: React.FC<LinkCardProps> = (props) => {
-  const { className } = props;
+  const { className, label, title, to } = props;
 
   return (
-    <div className="container mx-auto p-3">
-      <Link href={"#"} passHref>
-        <a
-          className={`flex flex-nowrap items-center p-4 lg:p-8 rounded-lg \
+    <Link href={to} passHref>
+      <a
+        className={`flex flex-nowrap items-center p-4 lg:p-8 rounded-lg \
                     bg-indigo-100 transition-shadow \
-                    group hover:shadow-card focus:outline-none active:bg-indigo-200 active:shadow-none`}
-        >
-          <span className={`flex-1 space-y-2 lg:space-y-5`}>
-            <Heading level="4" type="h6" fontSerif colorBlue>
-              Label
-            </Heading>
-            <Heading level="2" type="h5">
-              Title
-            </Heading>
-          </span>
-          <IconChevron
-            className={`flex-none w-2 h-2 lg:w-3 lg:h-3 transition-color`}
-          />
-        </a>
-      </Link>
-    </div>
+                    group hover:shadow-card focus:outline-none active:bg-indigo-200 active:shadow-none
+                    ${className ?? ""}`}
+      >
+        <span className={`flex-1 space-y-2 lg:space-y-5`}>
+          <Heading level="4" type="h6" fontSerif colorBlue>
+            {label}
+          </Heading>
+          <Heading level="2" type="h5">
+            {title}
+          </Heading>
+        </span>
+        <IconChevron
+          className={`flex-none w-2 h-2 lg:w-3 lg:h-3 transition-color`}
+        />
+      </a>
+    </Link>
   );
 };
 
